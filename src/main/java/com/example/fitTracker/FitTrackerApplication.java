a54cb622e99652d8628f740b56d9f9b8effabb21
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-04-24 19:55:24 +0100

    Created a run record for testing, all good

--- src/main/java/com/example/fitTracker/FitTrackerApplication.java
+++ src/main/java/com/example/fitTracker/FitTrackerApplication.java
@@ -1,9 +1,16 @@
 package com.example.fitTracker;
 
+import com.example.fitTracker.run.Location;
+import com.example.fitTracker.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class FitTrackerApplication {
@@ -12,8 +19,13 @@ public class FitTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitTrackerApplication.class, args);
-		log.info("Application running successfully!");
-
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "First Run,", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 6, Location.OUTDOOR);
+			log.info("Run: " + run);
+		};
+	}
 }
---
+++ src/main/java/com/example/fitTracker/run/Location.java
@@ -0,0 +1,5 @@
+package com.example.fitTracker.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
