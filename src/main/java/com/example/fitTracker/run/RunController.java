3f14b9744b97cb0a84142c88c539054864312f13
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-04-24 20:57:18 +0100

    Get all and get by id implemented in the runs

--- src/main/java/com/example/fitTracker/run/RunController.java
+++ src/main/java/com/example/fitTracker/run/RunController.java
@@ -1,13 +1,42 @@
 package com.example.fitTracker.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
-    @GetMapping("/hello")
-    String home() {
-        return "Hello, runner!";
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    // Get all runs
+    @GetMapping("")
+    List<Run> findAll() {
+        return runRepository.findAll();
+    }
+
+    // Get run by id
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+
+        if (run.isEmpty()) {
+           throw new  ResponseStatusException(HttpStatus.NOT_FOUND, "Run does not exist!");
+        }
+        return run.get();
+    }
+
+    // Create new run
+    @PostMapping("/create")
+    void create (@RequestBody Run run) {
+        runRepository.create(run);
     }
 }
--- src/main/java/com/example/fitTracker/run/RunRepository.java
+++ src/main/java/com/example/fitTracker/run/RunRepository.java
@@ -1,4 +1,49 @@
 package com.example.fitTracker.run;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id().equals(id))
+                .findFirst();
+    }
+
+    void create (Run run) {
+        runs.add(run);
+    }
+
+    // Adding some runs in-memory for now
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Morning Jog",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(2, ChronoUnit.HOURS),
+                10,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Night Jog",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(1, ChronoUnit.HOURS),
+                24,
+                Location.OUTDOOR));
+    }
+
 }
