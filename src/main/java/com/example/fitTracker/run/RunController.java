01baf6b791b9ec46928ff9ad472c6fb7a68a2d6d
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-05-07 18:57:26 +0100

    New run controllers on the repository with jdbcClient

--- src/main/java/com/example/fitTracker/run/RunController.java
+++ src/main/java/com/example/fitTracker/run/RunController.java
@@ -1,6 +1,5 @@
 package com.example.fitTracker.run;
 
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -23,36 +22,36 @@ public class RunController {
     List<Run> findAll() {
         return runRepository.findAll();
     }
-
-    // Get run by id
-    @GetMapping("/{id}")
-    Run findById(@PathVariable Integer id) {
-        Optional<Run> run = runRepository.findById(id);
-
-        if (run.isEmpty()) {
-           throw new RunNotFoundException();
-        }
-        return run.get();
-    }
-
-    // Create new run
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("")
-    void create (@Valid @RequestBody Run run) {
-        runRepository.create(run);
-    }
-
-    //Update a run by id
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{id}")
-    void update (@RequestBody Run run, @PathVariable Integer id) {
-        runRepository.update(run, id);
-    }
-
-    // Delete run by id
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    void delete (@PathVariable Integer id) {
-        runRepository.delete(id);
-    }
+//
+//    // Get run by id
+//    @GetMapping("/{id}")
+//    Run findById(@PathVariable Integer id) {
+//        Optional<Run> run = runRepository.findById(id);
+//
+//        if (run.isEmpty()) {
+//           throw new RunNotFoundException();
+//        }
+//        return run.get();
+//    }
+//
+//    // Create new run
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping("")
+//    void create (@Valid @RequestBody Run run) {
+//        runRepository.create(run);
+//    }
+//
+//    //Update a run by id
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @PutMapping("/{id}")
+//    void update (@RequestBody Run run, @PathVariable Integer id) {
+//        runRepository.update(run, id);
+//    }
+//
+//    // Delete run by id
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/{id}")
+//    void delete (@PathVariable Integer id) {
+//        runRepository.delete(id);
+//    }
 }
